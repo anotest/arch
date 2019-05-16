1d54ad8ba1dfd18cd52401bd8db8597015b1854f
anotest <50695476+anotest@users.noreply.github.com>
2019-05-16 12:16:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 set -xe
 timedatectl set-ntp true
 
@@ -43,13 +46,27 @@ mkdir /mnt/boot
 mount /dev/sda2 /mnt/boot
 mkdir /mnt/home 
 mount /dev/volgroup0/lv_home /mnt/home 
-pacstrap -i /mnt base base-devel
+pacstrap -i /mnt base
 genfstab -U -p /mnt >> /mnt/etc/fstab
-config(){
-    rm -rf /mnt/root/config.sh
-    wget https://raw.githubusercontent.com/anotest/arch/master/config.sh -O /mnt/root/config.sh
-    chmod +x /mnt/root/config.sh
-    arch-chroot /mnt /root/config.sh $ROOT $boot
-}
 
-config
+arch-chroot /mnt passwd
+arch-chroot /mnt useradd -m -g wheel joker 
+arch-chroot /mnt passwd joker
+arch-chroot /mnt sed -i 's/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/' /etc/sudoers
+arch-chroot /mnt pacman -S --noconfirm grub linux-headers ttf-dejavu i3 dmenu sddm networkmanager xorg-server 
+arch-chroot /mnt systemctl enable sddm 
+arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt echo 'LANG="en_US.UTF-8"' >> /etc/locale.conf
+arch-chroot /mnt echo "en_US.UTF-8" >> /etc/locale.gen
+arch-chroot /mnt locale-gen
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
+arch-chroot /mnt hwclock --systohc --localtime
+arch-chroot /mnt echo "KEYMAP=cz" > /etc/vconsole.conf
+arch-chroot /mnt echo arch > /etc/hostname
+arch-chroot /mnt cat > /etc/hosts <<EOF
+127.0.0.1 localhost.localdomain localhost arch
+::1 localhost.localdomain localhost arch
+EOF
+
+
+
