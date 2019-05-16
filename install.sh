e2572786b21e0b652de86b58ae7c98f822495ca7
anotest <50695476+anotest@users.noreply.github.com>
2019-05-16 07:54:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+set -xe
+timedatectl set-ntp true
+
 fdisk /dev/sda <<EOF
 o
 n
@@ -17,3 +20,16 @@ t
 8e
 w
 EOF
+setup_LVM {
+  cryptsetup luksFormat /dev/sda2
+  cryptsetup open --type luks /dev/sda2 lvm
+  pvcreate /dev/mapper/lvm
+  vgcreate volgroup0 /dev/mapper/lvm
+  lvcreate -L 3GB volgroup0 -n lv_root
+  lvcreate -L 1GB volgroup0 -n lv_swap
+  lvcreate -l 100%FREE volgroup0 -n lv_home
+  modprobe dm_mod
+ vgscan
+ vgchange -ay
+
+}
